ac8c124eb1793d9631e63d62417dbb176c9ece92
Exokem <ex010@live.com>
2021-07-24 16:33:19 -0400

    Modified SaplingBlockMixin to use Inject instead of overwriting

--- src/main/java/mixin/SaplingBlockMixin.java
+++ src/main/java/mixin/SaplingBlockMixin.java
@@ -10,6 +10,10 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import world.LargeOakTreeSaplingGenerator;
 
 import java.util.Random;
@@ -29,44 +33,38 @@ public class SaplingBlockMixin extends PlantBlock implements Fertilizable
     }
 
     @Shadow
-    @Override
     public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient)
     {
         return false;
     }
 
     @Shadow
-    @Override
     public boolean canGrow(World world, Random random, BlockPos pos, BlockState state)
     {
         return false;
     }
 
     @Shadow
-    @Override
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state)
     {
 
     }
 
-    public void generate(ServerWorld world, BlockPos pos, BlockState state, Random random)
+    @Inject
+    (
+        method = "generate(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Ljava/util/Random;)V",
+        at = @At("HEAD"), cancellable = true
+    )
+    public void injectGenerate(ServerWorld world, BlockPos pos, BlockState state, Random random, CallbackInfo ci)
     {
-        if (state.get(STAGE) == 0)
+        if (state.get(STAGE) != 0 && state.getBlock().equals(Blocks.OAK_SAPLING))
         {
-            world.setBlockState(pos, state.cycle(STAGE), 4);
-        }
+            LargeOakTreeSaplingGenerator.GENERATOR.generate
+                (
+                    world, world.getChunkManager().getChunkGenerator(), pos, state, random
+                );
 
-        else
-        {
-            if (state.getBlock().equals(Blocks.OAK_SAPLING))
-            {
-                LargeOakTreeSaplingGenerator.GENERATOR.generate
-                    (
-                        world, world.getChunkManager().getChunkGenerator(), pos, state, random
-                    );
-            }
-
-            else this.generator.generate(world, world.getChunkManager().getChunkGenerator(), pos, state, random);
+            ci.cancel();
         }
     }
 }
